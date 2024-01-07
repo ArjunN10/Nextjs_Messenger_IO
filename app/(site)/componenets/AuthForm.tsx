471dc3bc5d650fa5a98a36cc5fa09c11c87933d0
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2024-01-07 23:16:57 +0530

    Login Section

--- app/(site)/componenets/AuthForm.tsx
+++ app/(site)/componenets/AuthForm.tsx
@@ -1,150 +1,147 @@
-'use client';
+"use client";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import {BsGithub, BsGoogle} from 'react-icons/bs' 
 
 import Button from "@/app/components/Button";
 import Input from "@/app/components/Input";
-import { useCallback, useState } from "react"
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-type variant= 'Login' | 'Register'
-
-const AuthForm=()=>{
-
-const [variant,setvariant]=useState<variant>('Login');
-const [isLoading,setisLoading]=useState(false);
+import AuthSocialButton from "./AuthSocialButton";
 
 
-const togglevariant=useCallback(()=>{
-    if(variant === 'Login'){
-        setvariant('Register')
-    }else{
+type variant = "Login" | "Register";
 
-        setvariant('Login')
+const AuthForm = () => {
+  const [variant, setvariant] = useState<variant>("Login");
+  const [isLoading, setisLoading] = useState(false);
+
+  const togglevariant = useCallback(() => {
+    if (variant === "Login") {
+      setvariant("Register");
+    } else {
+      setvariant("Login");
     }
-},[variant])
+  }, [variant]);
 
-
-
-const {
+  const {
     register,
     handleSubmit,
-    formState:{
-        errors
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
     },
-}=useForm<FieldValues>({
-    defaultValues:{
-        name:'',
-        email:'',
-        password:''
-    }
-})
+  });
 
-
-
-const onSubmit:SubmitHandler<FieldValues>=(data)=>{
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setisLoading(true);
 
-    if(variant === 'Register'){
-        //Axios register
+    if (variant === "Register") {
+      //Axios register
     }
-    if(variant === 'Login'){
-        //NextAuth signin
+    if (variant === "Login") {
+      //NextAuth signin
     }
-}
+  };
 
+  const socialAction = (action: string) => {
+    setisLoading(true);
 
-const socialAction=(action:string)=>{
-setisLoading(true);
+    //NextAuth social signin
+  };
 
-//NextAuth social signin
-}
+  return (
+        <div
+            className="
+                mt-8
+                sm:mx-auto
+                sm:w-full
+                sm:max-w-md"
+                >
+        <div
+            className="
+                bg-white
+                px-4
+                py-8
+                shadow-md
+                sm:rounded-lg
+                sm:px-10"
+                >
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+            {variant === "Register" && (
+                    <Input
+                        id="name"
+                        label="name"
+                        register={register}
+                        errors={errors}
+                    />
+                    )}
+                    <Input
+                    id="emal"
+                    label="Email Address"
+                    type="email"
+                    register={register}
+                    errors={errors}
+                    />
+                    <Input
+                    id="password"
+                    label="Password"
+                    type="password"
+                    register={register}
+                    errors={errors}
+                    />
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "Login" ? "Sign in" : "Register"}
+            </Button>
+    </form>
+        <div className="mt-6">
+            <div className="relative">
+                <div
+                    className="
+                        absolute
+                        inset-0
+                        flex
+                        item-center"
+                    >
+                    <div
+                    className="
+                        w-full 
+                        border-t
+                        border-gray-300"
+                    />
+                </div>
+                    <div
+                        className="
+                        relative
+                        flex
+                        justify-center
+                        text-sm"
+                    >
+                    <span
+                        className="
+                        bg-white
+                        px-2
+                        text-gray-500"
+                    >
+                      Or continue with
+                    </span>
+              </div>
+            </div>
+            <div className="mt-6 flex gap-2">
+                <AuthSocialButton 
+                icon={BsGithub}
+                onclick={()=>socialAction('github')}
+                />
+                <AuthSocialButton 
+                icon={BsGoogle}
+                onclick={()=>socialAction('google')}/>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+  );
+};
 
-
-return(
-<>
-<div className="
-mt-8
-sm:mx-auto
-sm:w-full
-sm:max-w-md
-">
-<div className="
-bg-white
-px-4
-py-8
-shadow-md
-sm:rounded-lg
-sm:px-10
-">
-
-<form 
-className="space-y-6"
-onSubmit={handleSubmit(onSubmit)}
->
-{variant === 'Register' && (
-<Input 
-id="name" 
-label="name" 
-register={register}
-errors={errors}
-/>
-)}
-<Input 
-id="emal" 
-label="Email Address"
-type="email" 
-register={register}
-errors={errors}
-/><Input 
-id="password" 
-label="Password" 
-type="password"
-register={register}
-errors={errors}
-/>
-<Button 
-disabled={isLoading}
-fullWidth
-type="submit"
->
-    {variant === 'Login'? 'Sign in':'Register'}
-</Button>
-</form>
-<div className="mt-6">
-<div className="relative">
-<div
-className="
-absolute
-inset-0
-flex
-item-center"
->
-<div className="
-w-full 
-border-t
-border-gray-300"
->
-<div className="
-relative
-flex
-justify-center
-text-sm"
->
-<span className="
-bg-white
-px-2
-text-gray-500"
->
-Or continue with
-</span>
-</div>
-</div>
-</div>
-
-</div>
-</div>
-</div>
-</div>
-</>
-    )
-}
-
-export default AuthForm
+export default AuthForm;
