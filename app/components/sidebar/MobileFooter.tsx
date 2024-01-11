2f371139b6dd05e685a4e6208e97436777aa87c9
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2024-01-11 17:49:30 +0530

    User UI

--- app/components/sidebar/MobileFooter.tsx
+++ app/components/sidebar/MobileFooter.tsx
@@ -1,9 +1,42 @@
+'use client'
 import React from 'react'
+import useConversation from '@/app/hooks/useConversation'
+import useRoutes from '@/app/hooks/useRoutes'
+import MobileItems from './MobileItems'
+
 
 const MobileFooter = () => {
+  const routes=useRoutes()
+  const {isOpen}=useConversation()
+
+  if(isOpen){
+    return null;
+  }
+
   return (
-    <div>MobileFooter</div>
-  ) 
+    <div className='
+    fixed
+    justify-center
+    w-full
+    bottom-0
+    z-40
+    flex
+    items-center
+    bg-white
+    border-t-[1px]
+    lg:hidden
+    '>
+      {routes.map((route)=>{
+          <MobileItems
+          key={route.href}
+          href={route.href}
+          active={route.active}
+          icon={route.icon}
+          onclick={route.onclick}
+          />
+        })}
+    </div>
+  ); 
 }
 
 export default MobileFooter
---
+++ app/components/sidebar/MobileItems.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MobileItems = () => {
+  return (
+    <div>MobileItems</div>
+  )
+}
+
+export default MobileItems
