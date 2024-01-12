a7380e0e3aa2fd6b1223da5dbf8d73e784619392
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2024-01-12 12:44:31 +0530

    Current UserInfo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental:{
+        appDir:true,
+        swcPlugins:[
+            ["next-superjson-plugin",{}]
+        ]
+    }
+}
 
 module.exports = nextConfig
