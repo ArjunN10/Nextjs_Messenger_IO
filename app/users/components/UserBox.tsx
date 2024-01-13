821f623d0107f0834f97def230a528caa96fce59
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2024-01-13 18:15:03 +0530

    UserList Display

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -10,26 +10,27 @@ import { useCallback, useState } from "react"
 interface UserBoxProps{
     data:User
 }
-
 const UserBox:React.FC<UserBoxProps>=({
-data
+    data
 })=>{
+    
+    // console.log("data",data)
 
 const router=useRouter()
 const [isLoading,setisLoading]=useState(false)
 
 //start conv with user/on click
-const handleClick=useCallback(()=>{
-setisLoading(true)
+        const handleClick=useCallback(()=>{
+        setisLoading(true)
 
-axios.post('/api/conversations',{ 
-    userId:data.id
-})
-.then((data)=>{
-router.push(`/conversatio   ns/${data.data.id}`)
-})
-.finally(()=>setisLoading(false));
-},[data,router])
+        axios.post('/api/conversations',{ 
+            userId:data.id
+        })
+        .then((data)=>{
+        router.push(`/conversatio   ns/${data.data.id}`)
+        })
+        .finally(()=>setisLoading(false));
+        },[data,router])
 
 return(
 <div
@@ -45,9 +46,30 @@ p-3
 hover:bg-neutral-100
 rounded-lg
 transition
-cursor-pointer">
-<Avatar user={data}/>
+cursor-pointer"
+>
 
+<Avatar user={data}/>
+<div className="min-w-0 flex-1">
+<div className="focus:outline-none">
+    <div className="
+    justify-between
+    flex
+    item-center
+    mb-1
+    ">
+        <p className="
+        text-sm
+        font-medium
+        text-gray-900"
+        >
+            {data.name}
+
+        </p>
+    </div>
+
+</div>
+</div>
 </div>
     )
 }
