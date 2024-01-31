e8ba8b5917a018a2ddabd5bb9bd01006b2fb46b7
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2024-01-31 14:28:05 +0530

    pusher section

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -5,7 +5,7 @@ export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_SECRET!,
-  cluster:'ap2',
+  cluster: 'ap2',
   useTLS: true,
 });
 
@@ -16,6 +16,6 @@ export const pusherClient = new PusherClient(
     endpoint:'/api/pusher/auth',
     transport:'ajax'
     },
-    cluster:'ap2',
+    cluster: 'ap2',
   }
 );
